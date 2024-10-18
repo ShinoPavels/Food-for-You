fb587e0910a26348fdad4902beefc93e27ddb052
ShinoPavels <shinopavels.contact@gmail.com>
2024-10-18 12:09:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
@@ -7,14 +7,20 @@ import EnergyResultPage from './components/EnergyResultPage';
 import GenerateMealsPage from './components/GenerateMealsPage';
 
 function App() {
+  const [language, setLanguage] = useState('ENG'); // Lifting the language state up
+
+  const toggleLanguage = () => {
+    setLanguage(language === 'ENG' ? 'FR' : 'ENG');
+  };
+
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Header />
+        <Header language={language} toggleLanguage={toggleLanguage} />
         <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="/result" element={<EnergyResultPage />} />
-          <Route path="/generate-meals" element={<GenerateMealsPage />} />
+          <Route path="/" element={<MainContent language={language} />} />
+          <Route path="/result" element={<EnergyResultPage language={language} />} />
+          <Route path="/generate-meals" element={<GenerateMealsPage language={language} />} />
         </Routes>
         <Footer />
       </div>
